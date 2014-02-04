302d0db8eea35f49461a4f3ca2bb7d76b8f22059
Юрий Кривопалов <yk@yk-vostro>
2014-02-04 23:33:51 +0400

    add test_query; fix json parsing

--- Test/Zalora/Query.hs
+++ Test/Zalora/Query.hs
@@ -9,14 +9,15 @@ import Data.Aeson
 import Test.Zalora.Data
 
 import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as BSC
 import qualified Data.Text as T
 
 instance FromJSON Shoe where
     parseJSON (Object v) =
         Shoe <$> v .: "description"
              <*> v .: "color"
-             <*> v .: "size"
-             <*> ((Just . Blob . BS.pack) <$> (v .: "photo"))
+             <*> (read <$> v .: "size")
+             <*> ((Just . Blob . BSC.pack) <$> v .: "photo")
 
     parseJSON _ = mzero
 
@@ -24,14 +25,8 @@ instance ToJSON Shoe where
     toJSON s@Shoe{..} =
         object [ "description" .= shoeDescription
                , "color" .= shoeColor
-               , "size" .= shoeSize
+               , "size" .= show shoeSize
                , "photo" .= (convertPhoto $ shoePhoto)
                ]
-        where convertPhoto (Just (Blob b)) = toJSON $ BS.unpack b
+        where convertPhoto (Just (Blob b)) = toJSON $ BSC.unpack b
               convertPhoto _ = toJSON ("" :: String)
-
-instance ToJSON File where
-    toJSON file =
-        case file of
-            (Blob b) -> toJSON $ BS.unpack b
-            _ -> toJSON ("" :: String)
